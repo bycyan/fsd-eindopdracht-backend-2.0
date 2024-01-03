a0c865672a67bbe663675d69d3628874203c831a
Cyan Dalebout <cyan@wervelwindmedia.nl>
2024-01-03 19:24:25 +0100

    CorsConfig klaar

--- src/main/java/nl/fsd/eindopdracht/soundwwise/config/CorsConfig.java
+++ src/main/java/nl/fsd/eindopdracht/soundwwise/config/CorsConfig.java
@@ -1,4 +1,26 @@
 package nl.fsd.eindopdracht.soundwwise.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//instelling frontend & backend koppelen
+//geen @CrossOrigin vermelding boven iedere klasse
+//In security configuratie .cors() activeren
+
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
